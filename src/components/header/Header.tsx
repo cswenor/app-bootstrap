f09c2c4a114810676f12a534425b61bb4917a7fd
Chris Swenor <cswenor@gmail.com>
2024-12-15 11:42:50 -0500

    Cleaned up lint errors

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@
 import { useTheme } from 'next-themes';
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { useState, useEffect } from 'react';
-import { Heading, IconButton, Link, Container, Box, Flex } from '@radix-ui/themes';
+import { Heading, IconButton, Container, Box, Flex } from '@radix-ui/themes';
 import NextLink from 'next/link';
 
 export default function Header() {
